ea7df43dcca9e4f7a171794fdab9b53993009ff8
Elle Wilson <eleanorwilsonn@gmail.com>
2018-06-19 16:36:14 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < Sinatra::Base
 
   post '/recipes' do
     @recipes = @Recipe.create(params)
-    redirect to 'rshow'
+    redirect to '/show'
   end
 
   get '/recipes' do
